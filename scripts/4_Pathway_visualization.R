fd95d9918fe96e588325c599190ad72fee09f25f
Martina Summer-Kutmon <mkutmon@users.noreply.github.com>
2022-10-10 16:54:17 +0200

    update visualization script

--- scripts/4_Pathway_visualization.R
+++ scripts/4_Pathway_visualization.R
@@ -10,14 +10,19 @@ RCy3::cytoscapePing()
 #RCy3::installApp("enhancedGraphics")
 #RCy3::installApp("WikiPathways")
 
-DataWithAverages <- read.delim("DataWithAverages.txt")
-DataWithAverages <- tibble::rownames_to_column(DataWithAverages, "Ensembl")
+data <- read.table("data/normalized-data.tsv", sep = "\t", header=TRUE)
+data$HumanAverage<-apply(data[,3:7], 1, mean)
+data$RabbitAverage<-apply(data[,8:12], 1, mean)
+data$SheepAverage<-apply(data[,13:17], 1, mean)
+
 # Notch signaling & TFG beta
-pathways <- c("WP560","WP2884", "WP4357", "WP3", "WP268","WP4874")
+pathways <- c("WP268","WP560")
 
 for(p in pathways) {
   command <- paste0("wikipathways import-as-pathway id=",p)
   RCy3::commandsRun(command) 
-  RCy3::loadTableData(DataWithAverages, data.key.column = "Ensembl", table.key.column = "Ensembl")
+  RCy3::loadTableData(data, data.key.column = "ENSGID", table.key.column = "Ensembl")
   RCy3::setNodeCustomHeatMapChart(c("HumanAverage","SheepAverage","RabbitAverage"), slot = 2, style.name = "WikiPathways")
 }
+
+data[data$ENSGID == "ENSG00000168214",]
